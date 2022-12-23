29be8e30405e9693ac8b8cb92359d053627cb613
FantomeKill <tristan.robichaud@outlook.com>
2022-12-23 15:36:00 -0500

    2022 - j4 15/15

--- 2022/j4.cpp
+++ 2022/j4.cpp
@@ -4,76 +4,59 @@
 //
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <set>
-#include <tuple>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    //fast io
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int x,y,g, v = 0;
-    std::cin >> x;
-    set<pair<string, string>> s;
-    set<pair<string, string>> d;
-    while (x--){
-        string n1,n2;
-        cin >> n1 >> n2;
-        s.emplace(make_pair(n1,n2));
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int x;
+    cin >> x;
+    set<pair<string, string>> xs;
+    for (int i = 0; i < x; i++) {
+        string a, b;
+        cin >> a >> b;
+        xs.emplace(make_pair(min(a, b), max(a, b)));
     }
-    std::cin >> y;
-    while (y--){
-        string n1,n2;
-        cin >> n1 >> n2;
-        d.emplace(make_pair(n1,n2));
+    int y;
+    cin >> y;
+    set<pair<string, string>> ys;
+    for (int i = 0; i < y; i++) {
+        string a, b;
+        cin >> a >> b;
+        ys.emplace(make_pair(min(a, b), max(a, b)));
     }
-
-
-    std::cin >> g;
-
-    set<pair<string,string>> gs;
-
-    while (g--){
-        vector<string> group(3);
-        for (int i = 0; i < 3; ++i) {
-            cin >> group[i];
+    int g;
+    cin >> g;
+    set<pair<string, string>> xs_good;
+    set<pair<string, string>> ys_bad;
+    for (int it = 0; it < g; it++) {
+        vector<string> s(3);
+        for (int i = 0; i < 3; i++) {
+            cin >> s[i];
         }
-
-        for (auto& name : s) {
-//            cout << group[0] << " " << group[1] << " " << group[2] << endl;
-            if (std::find(group.begin(), group.end(), name.first) != group.end()){
-                if (std::find(group.begin(), group.end(), name.second) != group.end()){
-                    //
-                } else {
-                    gs.emplace(name);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j) {
+                    continue;
                 }
-            }
-            if (std::find(group.begin(), group.end(), name.second) != group.end()){
-                if (std::find(group.begin(), group.end(), name.first) != group.end()){
-                    //
-                } else {
-                    gs.emplace(name);
-                }
-            }
-        }
-
-        for (auto& name : d) {
-//            cout << group[0] << " " << group[1] << " " << group[2] << endl;
-            if (std::find(group.begin(), group.end(), name.first) != group.end()){
-                if (std::find(group.begin(), group.end(), name.second) != group.end()){
-                    gs.emplace(name);
-                } else {
+                pair<string, string> p = {min(s[i], s[j]), max(s[i], s[j])};
+                xs_good.insert(p);
+                if (ys.find(p) != ys.end()) {
+                    ys_bad.insert(p);
                 }
             }
         }
     }
-    for (const auto &item: gs) {
-        v++;
+    int ans = 0;
+    for (auto& p : xs) {
+        if (xs_good.find(p) == xs_good.end()) {
+            ans += 1;
+        }
     }
-    cout << v<< endl;
+    ans += ys_bad.size();
+    cout << ans << '\n';
     return 0;
 }
