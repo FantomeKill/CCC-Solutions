c58a50a2bf22ad694dce7e2e994d6f63438ed7e6
Toxo <53322384+FantomeKill@users.noreply.github.com>
2023-01-12 08:49:15 -0500

    2018 - j4 not tested

--- 2018/j4.cpp
+++ 2018/j4.cpp
@@ -1,39 +1,54 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-int n;
-
-void rotV(int ma[n]*){
-    int **m;
-    if (ma[0][0] > ma[0][n] && ma[0][0] < ma[n][0]){
-        // rotate 2d array
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                m[i][j] = ma[j][i];
-            }
-        }
-        rotV(m);
-    } else {
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                cout << ma[i][j] << " ";
-            }
-            cout << endl;
+void rotateMatrix(int** matrix, int n) {
+    for (int i = 0; i < n / 2; i++) {
+        for (int j = i; j < n - i - 1; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][n - i - 1];
+            matrix[j][n - i - 1] = matrix[n - i - 1][n - j - 1];
+            matrix[n - i - 1][n - j - 1] = matrix[n - j - 1][i];
+            matrix[n - j - 1][i] = temp;
         }
     }
 }
 
+bool isGood(int** matrix, int n) {
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n-1; j++)
+        {
+            if (matrix[i][j] > matrix[i][j+1] || matrix[i][j] > matrix[i+1][j]) return false;
+        }
+    }
+    return true;
+}
+
 int main() {
+    int n;
     cin >> n;
-    int matrix[n][n];
+
+    int** matrix = new int*[n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+    }
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> matrix[i][j];
         }
     }
-    rotV(matrix[n][n]);
+
+    while(!isGood(matrix, n)){
+        rotateMatrix(matrix,n);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
