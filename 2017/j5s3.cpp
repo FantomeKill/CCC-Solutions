180f3316e95dc85203e999bbe22a2b356825f2bc
Tristan Robichaud <tristan.robichaud@outlook.com>
2023-09-07 11:59:39 -0400

    update - readme

--- 2017/j5s3.cpp
+++ 2017/j5s3.cpp
@@ -3,7 +3,26 @@
 //
 
 #include <iostream>
+#include <algorithm>
 
 int main(){
+    int n;
+    std::cin >> n;
+    int a[n];
+    for (int i = 0; i < n; ++i){
+        std::cin >> a[i];
+    }
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++) {
+            if(a[i]>a[j]){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    int verif = a[0]+a[n];
+
+
     return 0;
 }
